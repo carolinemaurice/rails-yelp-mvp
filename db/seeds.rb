f1b4eef7b3a73811ad9a1f972d9467895e3c8e9f
Caroline MAURICE <caroline.maurice.henry@gmail.com>
2023-02-22 16:09:12 +0100

    rails-yelp-mvp v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+casa = { name: 'La Casa', address: 'Fenouillet', category: 'italian' }
+frite = { name: 'La Nouvelle Belgique ', address: 'Seilh', category: 'belgian' }
+tradition = { name: 'O Tradition', address: 'Lespinasse', category: 'french' }
+monde = { name: 'Au tour du monde', address: 'Saint Alban', category: 'chinese' }
+sushi = { name: 'Sendo Sushi', address: 'Bruguière', category: 'japanese' }
+
+attr_group = [casa, frite, tradition, monde, sushi]
+
+attr_group.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
